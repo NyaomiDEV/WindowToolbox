6743df76ce3f66dcb9a71e400f3a949994d52ad9
Naomi Calabretta <me@arytonex.pw>
2021-12-17 22:53:49 +0100

    declarations

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
-export function SetWindowPosition(hWnd: number, hWndInsertAfter: number, x: number, y: number, cx: number, cy: number, uFlags: number): boolean | undefined;
-export function ParentToDesktop(hWnd: number): boolean | undefined;
+export function SetWindowPosition(hWnd: Buffer, hWndInsertAfter: Buffer, x: number, y: number, cx: number, cy: number, uFlags: number): boolean | undefined;
+export function ParentToDesktop(hWnd: Buffer): boolean | undefined;
 
 export const HWND_BOTTOM = 1;
 export const HWND_NOTOPMOST = -2;
@@ -21,8 +21,8 @@ export const SWP_NOCOPYBITS = 0x0100;
 export const SWP_NOOWNERZORDER = 0x0200; /* Don't do owner Z ordering */
 export const SWP_NOSENDCHANGING = 0x0400; /* Don't send WM_WINDOWPOSCHANGING */
 
-export const SWP_DRAWFRAME = SWP_FRAMECHANGED;
-export const SWP_NOREPOSITION = SWP_NOOWNERZORDER;
+export const SWP_DRAWFRAME = 0x0020; // SWP_FRAMECHANGED
+export const SWP_NOREPOSITION = 0x0200; // SWP_NOOWNERZORDER
 
 /* WINVER >= 0x0400 */
 export const SWP_DEFERERASE = 0x2000; // same as SWP_DEFERDRAWING
