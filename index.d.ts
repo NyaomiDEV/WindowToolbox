88ab22f8bb7ffe4c6e50b70fa8543e3448b0fb23
Naomi Calabretta <me@arytonex.pw>
2021-12-18 01:36:38 +0100

    fuck it

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,1 @@
 export function SetWindowPosition(hWnd: Buffer, hWndInsertAfter: Buffer, x: number, y: number, cx: number, cy: number, uFlags: number): boolean | undefined;
-export function ParentToDesktop(hWnd: Buffer): boolean | undefined;
--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,8 @@ Napi::Value SetWindowPosition(Napi::CallbackInfo const& info){
 	if(info.Length() != 7)
 		return info.Env().Undefined();
 
-    HWND hWnd = (HWND)info[0].As<Napi::Buffer<unsigned long>>().Data();
-    HWND hWndInsertAfter = (HWND)info[1].As<Napi::Buffer<unsigned long>>().Data();
+    HWND hWnd = (HWND) *info[0].As<Napi::Buffer<unsigned long*>>().Data();
+    HWND hWndInsertAfter = (HWND) *info[1].As<Napi::Buffer<unsigned long*>>().Data();
     int x = info[2].As<Napi::Number>().Int32Value();
     int y = info[3].As<Napi::Number>().Int32Value();
     int cx = info[4].As<Napi::Number>().Int32Value();
@@ -14,36 +14,11 @@ Napi::Value SetWindowPosition(Napi::CallbackInfo const& info){
     UINT uFlags = info[6].As<Napi::Number>().Uint32Value();
 
     BOOL result = SetWindowPos(hWnd, hWndInsertAfter, x, y, cx, cy, uFlags);
-	return Napi::Boolean::New(info.Env(), result != 0);
-}
-
-Napi::Value ParentToDesktop(Napi::CallbackInfo const& info){
-    if (info.Length() != 1)
-        return info.Env().Undefined();
-
-    HWND hWnd = (HWND)info[0].As<Napi::Buffer<unsigned long>>().Data();
-    HWND desktop = GetDesktopWindow();
-    HWND hWorkerW = NULL;
-    HWND hShellViewWin = NULL;
-
-    do{
-        hWorkerW = FindWindowEx(desktop, hWorkerW, "WorkerW", NULL);
-        hShellViewWin = FindWindowEx(hWorkerW, 0, "SHELLDLL_DefView", 0);
-    } while (hShellViewWin == NULL && hWorkerW != NULL);
-
-    bool ok = true;
-    if (hShellViewWin == NULL)
-        ok = false;
-    else
-        SetWindowLongPtr(hWnd, -8, (LONG_PTR)hShellViewWin);
-
-    return Napi::Boolean::New(info.Env(), ok);
+	return Napi::Boolean::New(info.Env(), result == TRUE);
 }
 
 Napi::Object Init(Napi::Env env, Napi::Object exports){
     exports.Set("SetWindowPosition", Napi::Function::New(env, SetWindowPosition));
-    exports.Set("ParentToDesktop", Napi::Function::New(env, ParentToDesktop));
-    
     return exports;
 }
 
